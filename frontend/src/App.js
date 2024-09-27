322691396ac59242d5ef4fa81814b168ea889579
Ashiq807 <ashiqos807@gmail.com>
2024-09-27 21:14:02 +0530

    6th commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import Home from "./components/Home";
+import ProductDetails from "./components/product/ProductDetails";
 
 import "./App.css";
 
@@ -10,10 +12,12 @@ function App() {
   return (
     <>
       <Router>
+        <Toaster position="top-center" />
         <Header />
         <div className="container">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/product/:id" element={<ProductDetails />} />
           </Routes>
         </div>
         <Footer />
